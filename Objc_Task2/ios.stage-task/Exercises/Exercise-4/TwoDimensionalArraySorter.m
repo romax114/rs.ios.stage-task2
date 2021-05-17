193e836d2cebcfbbafa5773ee0e8eda10bf61ee9
romax114 <70773814+romax114@users.noreply.github.com>
2021-05-18 01:05:11 +0300

    feat: implement function solution

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,47 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+    NSArray *subarray;
+    NSArray *sortedArray = @[];
+    NSArray *singleSortedArray = @[];
+    NSMutableArray *commonArray = [[NSMutableArray alloc] init];
+    NSMutableArray *splitedArraysSorted;
+    
+    
+    if ([array count] != 0) {
+        
+        for (int i = 0; i < array.count; i++) {
+            
+            if ([[array objectAtIndex:i] isKindOfClass:[NSArray class]]) {
+                subarray = [array objectAtIndex:i];
+                [commonArray addObjectsFromArray:subarray];
+                
+                for (int j = 0; j < commonArray.count; j++) {
+                    
+                    if ([[commonArray objectAtIndex:j] isKindOfClass:[NSNumber class]]) {
+                        sortedArray = [commonArray sortedArrayUsingSelector:@selector(compare:)];
+                    } else if ([[commonArray objectAtIndex:j] isKindOfClass:[NSString class]]) {
+                        sortedArray = [commonArray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+                    } else  if ([[commonArray objectAtIndex:j] isKindOfClass:[NSNumber class]] && [[commonArray objectAtIndex:j] isKindOfClass:[NSString class]]) {
+                        
+                        for (int k = 0; k < subarray.count; k++) {
+                            
+                            if ([[subarray objectAtIndex:k] isKindOfClass:[NSNumber class]]) {
+                                singleSortedArray = [subarray sortedArrayUsingSelector:@selector(compare:)];
+                                [splitedArraysSorted addObject:singleSortedArray];
+                            } else if ([[subarray objectAtIndex:j] isKindOfClass:[NSString class]]) {
+                                singleSortedArray =[subarray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+                                [splitedArraysSorted addObject:singleSortedArray];
+                            }
+                            
+                            sortedArray = [splitedArraysSorted copy];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return sortedArray;
 }
-
 @end
