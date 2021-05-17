a188b34135c8614b0b66937d80ab683fbe9ba499
romax114 <70773814+romax114@users.noreply.github.com>
2021-05-18 00:55:39 +0300

    feat: implement function solution

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,18 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+ 
+    NSInteger count = 0;
+    
+    for (int i = 0; i < [array count]; i++) {
+        for (int j = 1 + i; j < [array count]; j++) {
+            if (abs([array[j] intValue] - [array[i] intValue]) == [number intValue] ) {
+                count ++;
+            }
+        }
+    }
+
+    return count;
 }
 
 @end
