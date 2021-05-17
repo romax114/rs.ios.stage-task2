7d2ead16b1ae963919d02d03f56e8797f6c7df2a
romax114 <70773814+romax114@users.noreply.github.com>
2021-05-18 00:51:26 +0300

    feat: implement function solution

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,18 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 number = n;
+    NSMutableString *binaryString = [[NSMutableString alloc] init];
+    NSMutableString *reversedString = [[NSMutableString alloc] init];
+    for (int i = 0; i < 8; i++) {
+        [binaryString insertString:(number & 1) ? @"1" : @"0"  atIndex:0];
+        number = number / 2;
+    }
+    for (int i = 0; i < 8; i++) {
+        NSString *buffer = [NSString stringWithFormat:@"%c", [binaryString characterAtIndex:(i)]];
+        [reversedString insertString:buffer atIndex:0];
+    }
+    number = strtol([reversedString UTF8String], NULL, 2);
+    
+    return number;
 }
