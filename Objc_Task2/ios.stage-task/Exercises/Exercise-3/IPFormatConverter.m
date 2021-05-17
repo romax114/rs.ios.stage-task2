414b8b9e061cb1ffe7a1e2098d0d6c84c5339bad
romax114 <70773814+romax114@users.noreply.github.com>
2021-05-18 01:00:11 +0300

    feat: implement function solution

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,31 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
-}
+    
+    if (!numbersArray || numbersArray.count == 0) {
+           return @"";
+       }
+       
+       NSDictionary *invalidNumber = @{@"invalidValue": @"The numbers in the input array can be in the range from 0 to 255.",
+                                       @"negativeValue": @"Negative numbers are not valid for input."};
+       
+       NSMutableString *address = [[NSMutableString alloc] init];
+       
+       for (NSInteger i = 0; i < 4; i++) {
+           NSInteger nextOct = 0;
+           if (i < numbersArray.count) {
+               nextOct = [numbersArray[i] intValue];
+           }
+           if (nextOct > 255) {
+               return invalidNumber[@"invalidValue"];
+           } else if (nextOct < 0) {
+               return invalidNumber[@"negativeValue"];
+           } else {
+               [address appendFormat:@"%ld.", nextOct];
+           }
+       }
+       
+       return [address substringToIndex:[address length] - 1];
+   }
 
 @end
